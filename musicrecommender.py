0e796df0d5dce31ee76c20d85c104fd4bf081bf3
Sai Thein Htay <theinhtay.thy@gmail.com>
2025-04-23 03:55:10 +0700

    SEVENTH PROJECT OF YOUTUBE

---
+++ musicrecommender.py
@@ -0,0 +1,14 @@
+import random
+
+print("\t\t\tMUSIC RECOMMENDER")
+genres = {
+    "rock": ["AC/DC","Queen","Led Zeppelin"],
+    "pop": ["Taylor Swift", "Ed Sheeran","Ariana Grande"],
+    "hip-hop": ["Kendrick Lamar","Drake","J. Cole"],
+}
+
+choice = input("What genre do you like ? (rock/pop/hip-hop: ) ")
+if choice not in genres:
+    print("Sorry, I don't know that genre.")
+else:
+    print(f"Check out {random.choice(genres[choice])}")
