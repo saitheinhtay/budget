33f4750de13e2c6dfc2dff733f2d392ae4ce444e
Sai Thein Htay <theinhtay.thy@gmail.com>
2025-04-24 18:26:35 +0700

    NINE PROJECT OF YOUTUBE

---
+++ reversename.py
@@ -0,0 +1,13 @@
+print("\t\t\tREVERSE NAME GENERATOR")
+while True:
+    name = input("\nEnter a name : ")
+
+    if not name:
+        break
+    reversed_name = name[::-1]
+    print(f"Your reversed name is : {reversed_name}")
+    print(f"In a parallel universe, they call you {reversed_name.title()}")
+
+    answer = input("\nTry another name ? (y/n): ")
+    if answer != "y":
+        break
